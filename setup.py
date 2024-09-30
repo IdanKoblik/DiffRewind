3452fc39854d0de99508c9eee53188790fde8d04
idan <idankob@gmail.com>
2024-09-30 10:57:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="diffdewind",
     version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
     install_requires=[
         "requests>=2.26.0",
     ],
