b7273d9de3607ac104843f7926f1e3ab91c77763
idan <idankob@gmail.com>
2024-09-30 11:21:06 +0300

    Update ci.yml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="diffdewind",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "requests>=2.26.0",
